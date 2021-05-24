de7191a24485463e8e403bd58fd9f404b0c5aacb
Terence Ro <rowyourboat83@gmail.com>
2021-05-24 08:47:17 -0500

    learning apollo provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,34 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Page>
-    <Component {...pageProps} />
-  </Page>
+import 'nprogress/nprogress.css';
+import { Component } from 'react';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+function MyApp({ Component, pageProps, apollo }) {
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
 }
+
+MyApp.getInitialProps = async ({ component, ctx }) => {
+  let pageProps = {};
+  if (component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+
+  return { pageProps };
+};
+
+export default withData(MyApp);
